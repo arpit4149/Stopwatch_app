03f8da50eee68f9b31c83eb26e6f69f86ff93566
arpit4149 <arpittiwari1392@gmail.com>
2024-12-15 19:02:45 +0530

    Add files via upload

---
+++ Stopwatch.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() {
+  runApp(StopwatchApp());
+}
+
+class StopwatchApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StopwatchHomePage(),
+    );
+  }
+}
+
+class StopwatchHomePage extends StatefulWidget {
+  @override
+  _StopwatchHomePageState createState() => _StopwatchHomePageState();
+}
+
+class _StopwatchHomePageState extends State<StopwatchHomePage> {
+  late Timer _timer;
+  Duration _elapsedTime = Duration.zero;
+  bool _isRunning = false;
+
+  void _startTimer() {
+    if (!_isRunning) {
+      _timer = Timer.periodic(Duration(seconds: 1), (timer) {
+        setState(() {
+          _elapsedTime += Duration(seconds: 1);
+        });
+      });
+      setState(() {
+        _isRunning = true;
+      });
+    }
+  }
+
+  void _stopTimer() {
+    if (_isRunning) {
+      _timer.cancel();
+      setState(() {
+        _isRunning = false;
+      });
+    }
+  }
+
+  void _resetTimer() {
+    _stopTimer();
+    setState(() {
+      _elapsedTime = Duration.zero;
+    });
+  }
+
+  String _formatDuration(Duration duration) {
+    String twoDigits(int n) => n.toString().padLeft(2, '0');
+    final hours = twoDigits(duration.inHours);
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return "$hours:$minutes:$seconds";
+  }
+
+  @override
+  void dispose() {
+    if (_isRunning) {
+      _timer.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Stopwatch App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _formatDuration(_elapsedTime),
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 40),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: _startTimer,
+                  child: Text('Start'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _stopTimer,
+                  child: Text('Stop'),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: _resetTimer,
+                  child: Text('Reset'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
